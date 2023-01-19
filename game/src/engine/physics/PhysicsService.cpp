244cebbb592573e4f712a6ee247c561aa71b8bfe
Suvrat Kumra <59862013+suvratkumra@users.noreply.github.com>
2023-01-19 08:12:03 -0700

    chore: Move all the physx stuff to the PhysicsService

--- game/src/engine/physics/PhysicsService.cpp
+++ game/src/engine/physics/PhysicsService.cpp
@@ -3,10 +3,49 @@
 #include "engine/core/debug/Log.h"
 #include "engine/service/ServiceProvider.h"
 
+#define PVD_HOST "127.0.0.1"
+
 using std::string_view;
 
 void PhysicsService::OnInit()
 {
+    Log::info("PhysicsService Initializing");
+
+    // initializing all the physx objects for use later.
+    initPhysX();
+}
+
+
+void PhysicsService::initPhysX()
+{
+    // PhysX init
+    Log::debug("Initializing PhysX object kFoundation");
+    kFoundation_ = PxCreateFoundation(PX_PHYSICS_VERSION, kDefaultAllocator_,
+        kDefaultErrorCallback_);
+    ASSERT_MSG(kFoundation_, "PhysX must be initialized");
+
+    //// For debugging purposes, initializing the physx visual debugger (download: https://developer.nvidia.com/gameworksdownload#)
+    kPvd_ = PxCreatePvd(*kFoundation_);       // create the instance of pvd
+    ASSERT_MSG(kPvd_, "Error initializing PhysX Visual Debugger");
+
+    physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
+    ASSERT_MSG(transport, "Error connecting to PhysX Visual Debugger");
+
+    kPvd_->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
+
+    // Physics initlaization
+    bool recordMemoryAllocations = true;
+    kPhysics_ = PxCreatePhysics(PX_PHYSICS_VERSION, *kFoundation_, physx::PxTolerancesScale(), recordMemoryAllocations, kPvd_);
+
+    // create default material
+    kMaterial_ = kPhysics_->createMaterial(0.5f, 0.5f, 0.6f);
+
+    physx::PxSceneDesc sceneDesc(kPhysics_->getTolerancesScale());
+    sceneDesc.gravity = physx::PxVec3(0.0f, -9.81f, 0.0f);
+    kDispatcher_ = physx::PxDefaultCpuDispatcherCreate(2);
+    sceneDesc.cpuDispatcher = kDispatcher_;
+    sceneDesc.filterShader = physx::PxDefaultSimulationFilterShader;
+    kScene_ = kPhysics_->createScene(sceneDesc);
 }
 
 void PhysicsService::OnStart(ServiceProvider& service_provider)
--- game/src/engine/physics/PhysicsService.h
+++ game/src/engine/physics/PhysicsService.h
@@ -1,14 +1,29 @@
 #pragma once
 
 #include "engine/service/Service.h"
+#include "PxPhysicsAPI.h"
+
 
 class PhysicsService final : public Service
 {
-  public:
+private:
+    void initPhysX();
+
+public:
     // From Service
     void OnInit() override;
     void OnStart(ServiceProvider& service_provider) override;
     void OnUpdate() override;
     void OnCleanup() override;
     std::string_view GetName() const override;
+
+private:
+    physx::PxDefaultAllocator kDefaultAllocator_;
+    physx::PxDefaultErrorCallback kDefaultErrorCallback_;
+    physx::PxFoundation* kFoundation_            = nullptr;
+    physx::PxPvd* kPvd_                          = nullptr;
+    physx::PxPhysics* kPhysics_                  = nullptr;
+    physx::PxMaterial* kMaterial_                = nullptr;
+    physx::PxScene* kScene_                      = nullptr;
+    physx::PxDefaultCpuDispatcher* kDispatcher_  = nullptr;
 };
