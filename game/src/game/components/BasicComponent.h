6fc3e82189087e6333f4e4548427317ed378a858
raduschirliu <radu.schirliu@gmail.com>
2023-01-20 11:14:57 -0700

    fix: remove OnUpdate from Components not using it

--- game/src/game/components/BasicComponent.h
+++ game/src/game/components/BasicComponent.h
@@ -7,6 +7,5 @@ class BasicComponent final : public Component
   public:
     // From Component
     void OnInit(const ServiceProvider& service_provider) override;
-    void OnUpdate() override;
     std::string_view GetName() const override;
 };
