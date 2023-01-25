da00a20409dcb014d63357cad0f9d6dec8dfcc2f
Radu Schirliu <radu.schirliu@gmail.com>
2023-01-25 12:02:21 -0700

    fix: cmake error

--- thirdparty/physx.cmake
+++ thirdparty/physx.cmake
@@ -8,8 +8,8 @@ set(PHYSX_BIN ${PHYSX_ROOT}/bin/win.x86_64.vc142.md)
 add_library(PhysX::PhysX64 SHARED IMPORTED GLOBAL)
 set_target_properties(PhysX::PhysX64
     PROPERTIES
-        IMPORTED_LOCATION_DEBUG ${PHYSX_BIN}/debug/PhysX_64.dll
-        IMPORTED_IMPLIB_DEBUG   ${PHYSX_BIN}/debug/PhysX_64.lib
+        IMPORTED_LOCATION ${PHYSX_BIN}/debug/PhysX_64.dll
+        IMPORTED_IMPLIB   ${PHYSX_BIN}/debug/PhysX_64.lib
 
         IMPORTED_LOCATION_RELEASE ${PHYSX_BIN}/release/PhysX_64.dll
         IMPORTED_IMPLIB_RELEASE   ${PHYSX_BIN}/release/PhysX_64.lib
@@ -18,8 +18,8 @@ set_target_properties(PhysX::PhysX64
 add_library(PhysX::PhysXCommon64 SHARED IMPORTED GLOBAL)
 set_target_properties(PhysX::PhysXCommon64
     PROPERTIES
-        IMPORTED_LOCATION_DEBUG ${PHYSX_BIN}/debug/PhysXCommon_64.dll
-        IMPORTED_IMPLIB_DEBUG   ${PHYSX_BIN}/debug/PhysXCommon_64.lib
+        IMPORTED_LOCATION ${PHYSX_BIN}/debug/PhysXCommon_64.dll
+        IMPORTED_IMPLIB   ${PHYSX_BIN}/debug/PhysXCommon_64.lib
 
         IMPORTED_LOCATION_RELEASE ${PHYSX_BIN}/release/PhysXCommon_64.dll
         IMPORTED_IMPLIB_RELEASE   ${PHYSX_BIN}/release/PhysXCommon_64.lib
@@ -28,8 +28,8 @@ set_target_properties(PhysX::PhysXCommon64
 add_library(PhysX::PhysXFoundation64 SHARED IMPORTED GLOBAL)
 set_target_properties(PhysX::PhysXFoundation64
     PROPERTIES
-        IMPORTED_LOCATION_DEBUG ${PHYSX_BIN}/debug/PhysXFoundation_64.dll
-        IMPORTED_IMPLIB_DEBUG   ${PHYSX_BIN}/debug/PhysXFoundation_64.lib
+        IMPORTED_LOCATION ${PHYSX_BIN}/debug/PhysXFoundation_64.dll
+        IMPORTED_IMPLIB   ${PHYSX_BIN}/debug/PhysXFoundation_64.lib
 
         IMPORTED_LOCATION_RELEASE ${PHYSX_BIN}/release/PhysXFoundation_64.dll
         IMPORTED_IMPLIB_RELEASE   ${PHYSX_BIN}/release/PhysXFoundation_64.lib
@@ -38,7 +38,7 @@ set_target_properties(PhysX::PhysXFoundation64
 add_library(PhysX::PhysXPvdSDKStatic64 STATIC IMPORTED GLOBAL)
 set_target_properties(PhysX::PhysXPvdSDKStatic64
     PROPERTIES
-        IMPORTED_LOCATION_DEBUG ${PHYSX_BIN}/debug/PhysXPvdSDK_static_64.lib
+        IMPORTED_LOCATION ${PHYSX_BIN}/debug/PhysXPvdSDK_static_64.lib
 
         IMPORTED_LOCATION_RELEASE ${PHYSX_BIN}/release/PhysXPvdSDK_static_64.lib
 )
@@ -46,7 +46,7 @@ set_target_properties(PhysX::PhysXPvdSDKStatic64
 add_library(PhysX::PhysXExtensions64 STATIC IMPORTED GLOBAL)
 set_target_properties(PhysX::PhysXExtensions64
     PROPERTIES
-        IMPORTED_LOCATION_DEBUG ${PHYSX_BIN}/debug/PhysXExtensions_static_64.lib
+        IMPORTED_LOCATION ${PHYSX_BIN}/debug/PhysXExtensions_static_64.lib
         
         IMPORTED_LOCATION_RELEASE ${PHYSX_BIN}/release/PhysXExtensions_static_64.lib
 )
