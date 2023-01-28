a741e452b0f34e94f4fb6cc3bfe18c10af03287d
JP <jullianpalma2001@gmail.com>
2023-01-28 15:30:01 -0700

    minor changes + now returns vehicle data struct

--- game/src/engine/config/ConfigService.cpp
+++ game/src/engine/config/ConfigService.cpp
@@ -6,29 +6,27 @@
 #include "engine/config/ConfigService.h"
 #include "engine/core/debug/Log.h"
 #include "engine/service/Service.h"
+#include "VehicleData.h"
 
 namespace fs = std::filesystem;
+
+ConfigService::ConfigService() : 
+    config_files(),
+    path("resources/config/")
+{}
 
 void ConfigService::OnInit()
 {
     Log::info("[ConfigService] Initializing");
     
-    std::vector<YAML::Node> config_files(0);
-    fs::path path ="resources/config/";
-    
+    // add all files in "config/" folder to a vector  
     for (const fs::directory_entry& entry : fs::directory_iterator(path))
     {
-        Log::debug("[ConfigService] Found file: {}", entry.path().string());
+        Log::info("[ConfigService] Found file: {}", entry.path().string());
         config_files.push_back(YAML::LoadFile(entry.path().string()));
     }
 
-    // parse testing
-    // for (YAML::Node file : config_files) {
-    //     YAML::Node number = file["number"];
-    //     YAML::Node numbers = file["numbers"];
-    //     YAML::Node string = file["string"];
-    //     YAML::Node strings = file["strings"];
-    // }
+    
 }
 
 void ConfigService::OnStart(ServiceProvider& service_provider)
@@ -44,6 +42,12 @@ void ConfigService::OnUpdate()
 void ConfigService::OnCleanup()
 {
 
+}
+
+VehicleData getVehicleData() 
+{
+    VehicleData vehicleData;    // currently does literally NOTHING
+    return vehicleData;
 }
 
 std::string_view ConfigService::GetName() const
--- game/src/engine/config/ConfigService.h
+++ game/src/engine/config/ConfigService.h
@@ -1,13 +1,24 @@
 #pragma once
 
-#include "engine/service/Service.h"
+#include <filesystem>
 
+#include <yaml-cpp/yaml.h>
+#include "engine/service/Service.h"
+#include "VehicleData.h"
+
+namespace fs = std::filesystem;
 class ConfigService final : public Service
 {
     public:
+        ConfigService();
         void OnInit() override;
         void OnStart(ServiceProvider& service_provider) override;
         void OnUpdate() override;
         void OnCleanup() override;
+        VehicleData getVehicleData();
         std::string_view GetName() const override;
+    
+    private:
+        std::vector<YAML::Node> config_files;
+        fs::path path;
 };
