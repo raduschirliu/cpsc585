2ec25f2a30e4943778d7a4bad79fc644867e76bf
JP <jullianpalma2001@gmail.com>
2023-02-06 13:25:32 -0700

    feat: create raycast data structure

---
+++ game/src/engine/physics/RaycastData.cpp
@@ -0,0 +1,21 @@
+#include "RaycastData.h"
+
+#include <glm/glm.hpp>
+
+#include "PxPhysicsAPI.h"
+#include "engine/core/math/Physx.h"
+
+using physx::PxRaycastBuffer;
+
+RaycastData::RaycastData(PxRaycastBuffer raycast_result)
+{
+    RaycastData::position = glm::vec3(raycast_result.block.position.x,
+                                      raycast_result.block.position.y,
+                                      raycast_result.block.position.z);
+
+    RaycastData::normal = glm::vec3(raycast_result.block.normal.x,
+                                    raycast_result.block.normal.y,
+                                    raycast_result.block.normal.z);
+
+    RaycastData::distance = raycast_result.block.distance;
+}
