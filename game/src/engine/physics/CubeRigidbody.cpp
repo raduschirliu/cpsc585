bcf144b031744890d49d3923ad431ec1ffbd8000
JP <jullianpalma2001@gmail.com>
2023-02-07 21:42:49 -0700

    feat: make the cube move a tad bit slower

--- game/src/engine/physics/CubeRigidbody.cpp
+++ game/src/engine/physics/CubeRigidbody.cpp
@@ -27,19 +27,19 @@ void CubeRigidbody::OnUpdate(const Timestep& delta_time)
         // While the sphere is getting updated, update the transform location.
         if (input_service_->IsKeyDown(GLFW_KEY_UP))
         {
-            transform_->Translate(glm::vec3(0.f, 0.f, -1.f));
+            transform_->Translate(glm::vec3(0.f, 0.f, -0.25f));
         }
         if (input_service_->IsKeyDown(GLFW_KEY_DOWN))
         {
-            transform_->Translate(glm::vec3(0.f, 0.f, 1.f));
+            transform_->Translate(glm::vec3(0.f, 0.f, 0.25f));
         }
         if (input_service_->IsKeyDown(GLFW_KEY_LEFT))
         {
-            transform_->Translate(glm::vec3(-1.f, 0.f, 0.f));
+            transform_->Translate(glm::vec3(-0.25f, 0.f, 0.f));
         }
         if (input_service_->IsKeyDown(GLFW_KEY_RIGHT))
         {
-            transform_->Translate(glm::vec3(1.f, 0.f, 0.f));
+            transform_->Translate(glm::vec3(0.25f, 0.f, 0.f));
         }
         // updating the position of the cube
         if (dynamic_)
