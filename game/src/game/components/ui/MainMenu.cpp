ead0ca8fba8e082f8c8aaab2a3b58a2ea1e65aea
Radu Schirliu <radu.schirliu@gmail.com>
2023-03-13 12:18:16 -0600

    feat: main menu example

---
+++ game/src/game/components/ui/MainMenu.cpp
@@ -0,0 +1,91 @@
+#include "game/components/ui/MainMenu.h"
+
+#include <imgui.h>
+
+#include "engine/core/debug/Log.h"
+#include "engine/core/gfx/Texture.h"
+#include "engine/scene/Entity.h"
+#include "engine/scene/SceneDebugService.h"
+#include "game/services/GameStateService.h"
+
+using std::make_unique;
+using std::string;
+using std::string_view;
+
+void MainMenu::OnInit(const ServiceProvider& service_provider)
+{
+    // Dependencies
+    input_service_ = &service_provider.GetService<InputService>();
+    game_state_service_ = &service_provider.GetService<GameStateService>();
+    scene_service_ = &service_provider.GetService<SceneDebugService>();
+
+    counter = 0;
+    logo_ = make_unique<Texture>("resources/textures/ui/logo.png");
+    button_ = make_unique<Texture>("resources/textures/ui/terrible_button.png");
+
+    // Events
+    GetEventBus().Subscribe<OnGuiEvent>(this);
+}
+
+string_view MainMenu::GetName() const
+{
+    return "MainMenu";
+}
+
+/*
+    Example of all the different ImGui controls can be found by pressing
+        F1 -> Show ImGui Example
+    In the debug UI, there's also a tool for seeing different styles:
+        tools -> style editor
+    The code for this menu is here if you wanna see how a certain part of it is
+   done: thirdparty/imgui/imgui_demo.
+    Documentation here: https://github.com/ocornut/imgui/wiki
+ */
+
+void MainMenu::OnGui()
+{
+    // Configure where the window will be placed first, since we'll make it
+    // non-movable
+    ImGuiWindowFlags flags = ImGuiWindowFlags_NoMove |
+                             ImGuiWindowFlags_AlwaysAutoResize |
+                             ImGuiWindowFlags_NoDecoration;
+    ImGui::SetNextWindowPos(ImVec2(120, 40));
+
+    const ImVec2& screen_size = ImGui::GetIO().DisplaySize;
+
+    // ImGui::Begin() ... ImGui::End() defines a window
+    // All calls to do ImGui stuff should be between these two
+    ImGui::Begin("Main menu", nullptr, flags);
+
+    // This works just like printf(), and has the same format specifiers: %f,
+    // %s, etc.
+    string some_string = "hello again";
+    ImGui::Text("hello world: %d %s", 1234, some_string.c_str());
+
+    ImGui::Text("Your game window is: %fpx x %fpx", screen_size.x,
+                screen_size.y);
+
+    if (ImGui::Button("a nice button"))
+    {
+        // This if statement gets ran whenever the button is clicked
+        counter++;
+    }
+
+    ImGui::Image(logo_->GetGuiHandle(), ImVec2(200, 200));
+
+    if (ImGui::ImageButton("terrible button", button_->GetGuiHandle(),
+                           ImVec2(125, 50)))
+    {
+        // Has to be the name of a scene defined near the top of GameApp.cpp
+        scene_service_->SetActiveScene("Track1");
+    }
+
+    ImGui::Text("Button click counter: %d", counter);
+
+    // Seting color for only a few elements
+    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.1f, 1.0f, 1.0f, 1.0f));
+    ImGui::Button("useless but styled button");
+    ImGui::PopStyleColor(1);
+
+    ImGui::End();
+}
