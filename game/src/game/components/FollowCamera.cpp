c64d9c51df2605ff53fe1565ea0faf64768d6401
Mihailo Jankovic <mihailo.jankovic@ucalgary.ca>
2023-03-27 11:33:14 -0600

    changed cam

--- game/src/game/components/FollowCamera.cpp
+++ game/src/game/components/FollowCamera.cpp
@@ -17,9 +17,9 @@ using glm::vec3;
 
 FollowCamera::FollowCamera()
     : offset_(0.0f, 10.0f, 0.0f),
-      distance_(23.0f),
-      orientation_lerp_factor_(2.5f),
-      position_lerp_factor_(2.5f)
+      distance_(21.0f),
+      orientation_lerp_factor_(2.8f),
+      position_lerp_factor_(2.8f)
 {
 }
 
