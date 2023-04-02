0451146d5b315d171c54cb59dd2a8c736d3d766f
Mihailo Jankovic <mihailo.jankovic@ucalgary.ca>
2023-04-02 13:43:37 -0600

    Started on minimap
    Basically just gonna make a camera follow from above looking down on track from afar.

---
+++ game/src/game/components/ui/Minimap.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <object_ptr.hpp>
+
+#include "engine/scene/Component.h"
+#include "engine/scene/OnUpdateEvent.h"
+#include "engine/scene/Transform.h"
+
+class Minimap : public Component, public IEventSubscriber<OnUpdateEvent>
+{
+  public:
+    Minimap();
+
+    // From Component
+    void OnInit(const ServiceProvider& service_provider) override;
+    void OnDebugGui() override;
+    std::string_view GetName() const override;
+
+    // From IEventSubscriber<OnUpdateEvent>
+    void OnUpdate(const Timestep& delta_time) override;
+
+    // Set the entity from which we want to follow using this camera
+    void SetFollowingTransform(Entity& entity);
+    void SetCameraOffset(glm::vec3 offset);
+
+  private:
+    jss::object_ptr<Transform> transform_;
+    jss::object_ptr<Transform> target_transform_;
+    jss::object_ptr<VehicleComponent> target_vehicle_;
+
+    glm::vec3 offset_;
+    float distance_;
+    float orientation_lerp_factor_;
+    float position_lerp_factor_;
+};
