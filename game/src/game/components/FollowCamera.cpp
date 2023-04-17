fd0029e374019f4f9be2a8ef443b129ef75a14b7
Radu Schirliu <radu.schirliu@gmail.com>
2023-04-17 12:11:00 -0600

    fix: exhaust particle speed

--- game/src/game/components/FollowCamera.cpp
+++ game/src/game/components/FollowCamera.cpp
@@ -8,6 +8,7 @@
 #include "engine/render/Camera.h"
 #include "engine/render/RenderService.h"
 #include "engine/scene/Entity.h"
+#include "engine/scene/Transform.h"
 #include "engine/service/ServiceProvider.h"
 #include "game/components/VehicleComponent.h"
 #include "game/components/state/PlayerState.h"
@@ -17,13 +18,18 @@ using glm::quat;
 using glm::vec3;
 using glm::vec4;
 
+static constexpr float kBaseFovDeg = 90.0f;
+static constexpr float kMaxFovDeg = 105.0f;
+
 FollowCamera::FollowCamera()
     : offset_(0.0f, 8.0f, 0.0f),
       lookat_offset_(0.0f, 5.0f, 0.0f),
       distance_(25.0f),
       orientation_lerp_factor_(20.0f),
       position_lerp_factor_(5.0f),
-      acceleration_factor_(20.0f)
+      acceleration_factor_(20.0f),
+      fov_factor_(50.0f),
+      fov_offset_(0.0f)
 {
 }
 
@@ -31,6 +37,7 @@ void FollowCamera::OnInit(const ServiceProvider& service_provider)
 {
     // Dependencies
     transform_ = &GetEntity().GetComponent<Transform>();
+    camera_ = &GetEntity().GetComponent<Camera>();
 
     // Event subscriptions
     GetEventBus().Subscribe<OnUpdateEvent>(this);
@@ -67,6 +74,9 @@ void FollowCamera::OnUpdate(const Timestep& delta_time)
     const float tilt_angle =
         20.0f * (player_state_->GetCurrentSpeed() / 130.0f);
     const float oscillation = sin(glm::radians(tilt_angle));
+    const float fov_increase =
+        glm::clamp(oscillation * fov_factor_ - fov_offset_, 0.0f,
+                   kMaxFovDeg - kBaseFovDeg);
     const float tilt_amount = oscillation * acceleration_factor_;
 
     target_position.y += tilt_amount;
@@ -85,6 +95,8 @@ void FollowCamera::OnUpdate(const Timestep& delta_time)
     transform_->SlerpOrientation(target_orientation,
                                  orientation_lerp_factor_ * dt_sec);
     transform_->LerpPosition(camera_position, position_lerp_factor_ * dt_sec);
+
+    camera_->SetFov(kBaseFovDeg + fov_increase);
 }
 
 void FollowCamera::SetPlayerState(PlayerState& player_state)
@@ -101,6 +113,8 @@ void FollowCamera::OnDebugGui()
     gui::EditProperty("Offset", offset_);
     gui::EditProperty("Lookat Offset", lookat_offset_);
     ImGui::DragFloat("Distance", &distance_, 1.0f, -100.0f, 100.0f);
+    ImGui::DragFloat("Fov Factor", &fov_factor_, 1.0f, -100.0f, 100.0f);
+    ImGui::DragFloat("Fov Offset", &fov_offset_, 1.0f, -100.0f, 100.0f);
     ImGui::DragFloat("Orientation Lerp Factor", &orientation_lerp_factor_,
                      0.05f, 0.0f, 10.0f);
     ImGui::DragFloat("Position Lerp Factor", &position_lerp_factor_, 0.05f,
